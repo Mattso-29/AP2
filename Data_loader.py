0cfb37f63c314a6d7dc220d99cd72bca7fb57ab9
Mattso-29 <160502450+Mattso-29@users.noreply.github.com>
2024-05-22 20:07:18 +0200

    Update Data_loader.py

--- Data_loader.py
+++ Data_loader.py
@@ -10,8 +10,8 @@ def load_data(file_path, columns_to_drop, start_date):
     return df
 
 def load_all_data():
-    france = load_data('France.xlsx', [1, 4], '2000-01-01')
-    germany = load_data('Allemagne.xlsx', [2], '2000-01-01')
-    switzerland = load_data('Suisse.xlsx', [1], '2000-01-01')
-    portugal = load_data('Portugal.xlsx', [], '2000-01-01')
+    france = load_data('France copy.xlsx', [1, 4], '2000-01-01')
+    germany = load_data('Allemagne copy.xlsx', [2], '2000-01-01')
+    switzerland = load_data('Suisse copy.xlsx', [1], '2000-01-01')
+    portugal = load_data('Portugal copy.xlsx', [], '2000-01-01')
     return france, germany, switzerland, portugal
