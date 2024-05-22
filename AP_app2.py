002a34f3d296f60d59bb1f1d665067bb329d163d
Mattso-29 <160502450+Mattso-29@users.noreply.github.com>
2024-05-22 20:03:59 +0200

    Update AP_app2.py

--- AP_app2.py
+++ AP_app2.py
@@ -4,7 +4,7 @@ from streamlit_folium import st_folium
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from data_loader import load_all_data
+from Data_loader import load_all_data
 
 france, germany, switzerland, portugal = load_all_data()
 
