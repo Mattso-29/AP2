3c8fabe2f6df3016f77d74f51418dd9d9686f15f
Mattso-29 <160502450+Mattso-29@users.noreply.github.com>
2024-05-23 16:06:06 +0200

    Update map_loader.py

--- map_loader.py
+++ map_loader.py
@@ -442,22 +442,4 @@ center_coords = {
     "Switzerland 🇨🇭": [46.818188, 10.227512]  
 }
 
-def display_map(selected_country):
-    center = center_coords.get(selected_country, [48.8566, 2.3522])
-    zoom_start = 6 if selected_country else 4
 
-    m = folium.Map(location=center, zoom_start=zoom_start, tiles='CartoDB positron')
-
-    for country, geojson in geojson_data.items():
-        folium.GeoJson(
-            geojson,
-            style_function=lambda x, country=country: {
-                'color': 'black' if country == selected_country else 'gray',
-                'fillColor': 'black' if country == selected_country else 'darkgray',
-                'fillOpacity': 0.7
-            },
-            highlight_function=lambda x: {'weight': 3, 'color': 'black'},
-            tooltip=folium.Tooltip(country)
-        ).add_to(m)
-
-    st_folium(m, width=1400, height=800)
